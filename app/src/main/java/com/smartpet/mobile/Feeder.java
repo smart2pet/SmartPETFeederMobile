e722c725009788a45b90a9ed593819bf3618612e
smart2pet <smart_pet@126.com>
2022-12-26 19:27:23 +0800

    Initial commit

---
+++ app/src/main/java/com/smartpet/mobile/Feeder.java
@@ -0,0 +1,163 @@
+package com.smartpet.mobile;
+
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+class Failed extends Exception {
+    public Failed(String message) {
+        super(message);
+    }
+}
+
+public class Feeder {
+    String ip;
+    String uri;
+    public Feeder(String ip) throws Failed {
+        try {
+            HttpURLConnection conn = null;
+            URL url = new URL("http://" + ip + ":8000");
+            this.uri = "http://" + ip + ":8000";
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if (conn.getResponseCode() == 200) {
+                this.ip = ip;
+            } else {
+                throw (new Failed("Connect failed. "));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+//            throw (new IOException());
+            throw new Failed("Connect failed. ");
+        }
+    }
+
+    public void addFeedingPlan(int hour, int minute, int weight) throws Failed {
+        try {
+            StringBuffer sbf = new StringBuffer();
+            String strRead = null;
+            HttpURLConnection conn = null;
+            URL url = new URL(uri + "/api/plan");
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            conn.connect();
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8);
+            writer.write("{\"time_h\":");
+            writer.write(String.valueOf(hour));
+            writer.write(",");
+            writer.write("\"time_m\":");
+            writer.write(String.valueOf(minute));
+            writer.write(",");
+            writer.write("\"weight\":");
+            writer.write(String.valueOf(weight));
+            writer.write("}");
+            writer.flush();
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+            }
+            reader.close();
+            conn.disconnect();
+            String results = sbf.toString();
+            Log.println(Log.WARN, "FEed", results);
+            if (!results.equals("{\"result\":0}")) {
+                throw new Failed("Server returned failed result.");
+            } else {
+                Log.println(Log.WARN, "Feeder", "Add feeding plan request sent.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Failed("");
+        }
+    }
+
+    public void feed(int weight) throws Failed {
+        try {
+            StringBuffer sbf = new StringBuffer();
+            String strRead = null;
+            HttpURLConnection conn = null;
+            URL url = new URL(uri + "/api/food");
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            conn.connect();
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8);
+            writer.write("{\"weight\": " + String.valueOf(weight) + "}");
+            writer.flush();
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+            }
+            reader.close();
+            conn.disconnect();
+            String results = sbf.toString();
+            Log.println(Log.WARN, "FEed", results);
+            if (!results.equals("{\"result\":0}")) {
+                throw new Failed("Server returned failed result.");
+            } else {
+                Log.println(Log.WARN, "Feeder", "Feeding request sent.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Failed("Internal error.");
+        }
+    }
+    public void delFeedingPlan(int hour, int minute) throws Failed {
+        try {
+            StringBuffer sbf = new StringBuffer();
+            String strRead = null;
+            HttpURLConnection conn = null;
+            URL url = new URL(uri + "/api/plan");
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("DELETE");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            conn.connect();
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8);
+            writer.write("{\"time_h\":");
+            writer.write(String.valueOf(hour));
+            writer.write(",");
+            writer.write("\"time_m\":");
+            writer.write(String.valueOf(minute));
+            writer.write("}");
+            writer.flush();
+            InputStream is = conn.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+            }
+            reader.close();
+            conn.disconnect();
+            String results = sbf.toString();
+            Log.println(Log.WARN, "FEed", results);
+            if (!results.equals("{\"result\":0}")) {
+                throw new Failed("Server returned failed result.");
+            } else {
+                Log.println(Log.WARN, "Feeder", "Add feeding plan request sent.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new Failed("");
+        }
+    }
+}
---
+++ app/src/main/java/com/smartpet/mobile/MainActivity.java
@@ -0,0 +1,152 @@
+package com.smartpet.mobile;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.smartpet.mobile.databinding.ActivityMainBinding;
+import com.smartpet.mobile.ui.home.HomeFragment;
+import com.smartpet.mobile.Feeder;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    public Feeder feeder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+//        try {
+//            feeder = new Feeder("192.168.1.6:8000");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
+    public void feed(View view) {
+        int weight;
+        Object test;
+        EditText input;
+        input = (EditText) findViewById(R.id.inputFeedWeight);
+        String weight_string = input.getText().toString();
+        try {
+            weight = Integer.parseInt(weight_string);
+            Log.println(Log.WARN, "FEED", String.valueOf(weight));
+            feeder.feed(weight);
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Completed");
+            normalDialog.setMessage("Successful sent feeding request to feeder. ");
+            normalDialog.show();
+        } catch (NumberFormatException | Failed | NullPointerException e) {
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Error");
+            normalDialog.setMessage("Something went wrong... ");
+            normalDialog.show();
+            e.printStackTrace();
+        }
+    }
+    public void addPlan(View view) {
+        int hour, minute, weight;
+        EditText input_time, input_weight;
+        input_time = (EditText) findViewById(R.id.setTime);
+        String time_string = input_time.getText().toString();
+        try {
+            hour = Integer.parseInt(time_string.split(":")[0]);
+            minute = Integer.parseInt(time_string.split(":")[1]);
+        } catch (Exception ignored){
+            return;
+        }
+        input_weight = (EditText) findViewById(R.id.planWeight);
+        String weight_string = input_weight.getText().toString();
+//        weight = Integer.parseInt(weight_string);
+        try {
+            weight = Integer.parseInt(weight_string);
+            feeder.addFeedingPlan(hour, minute, weight);
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Completed");
+            normalDialog.setMessage("Successful sent add plan request to feeder. ");
+            normalDialog.show();
+
+        } catch (Failed | NumberFormatException | NullPointerException e){
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Error");
+            normalDialog.setMessage("Something went wrong... ");
+            normalDialog.show();
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    public void connect(View view) {
+        String ip;
+        EditText ip_edit;
+        TextView status;
+        ip_edit = (EditText) findViewById(R.id.ip);
+        status = (TextView) findViewById(R.id.status);
+        ip = ip_edit.getText().toString();
+        try {
+            this.feeder = new Feeder(ip);
+            status.setText("Connected!");
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Completed");
+            normalDialog.setMessage("Successful connected. ");
+            normalDialog.show();
+        } catch (Failed ignored) {}
+    }
+    public void delPlan(View view) {
+        int hour, minute;
+        EditText input_time;
+        input_time = (EditText) findViewById(R.id.setTime);
+        String time_string = input_time.getText().toString();
+        try {
+            hour = Integer.parseInt(time_string.split(":")[0]);
+            minute = Integer.parseInt(time_string.split(":")[1]);
+        } catch (Exception ignored){
+            return;
+        }
+//        input_weight = (EditText) findViewById(R.id.planWeight);
+//        String weight_string = input_weight.getText().toString();
+//        weight = Integer.parseInt(weight_string);
+        try {
+//            weight = Integer.parseInt(weight_string);
+            feeder.delFeedingPlan(hour, minute);
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Completed");
+            normalDialog.setMessage("Successful sent a delete plan request to feeder. ");
+            normalDialog.show();
+
+        } catch (Failed | NumberFormatException | NullPointerException e){
+            final AlertDialog.Builder normalDialog = new AlertDialog.Builder(MainActivity.this);
+            normalDialog.setTitle("Error");
+            normalDialog.setMessage("Something went wrong... ");
+            normalDialog.show();
+        }
+    }
+}
